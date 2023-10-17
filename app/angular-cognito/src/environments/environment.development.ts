15f0cec87e2e5d0649b3291d8224d55360496533
maxa2 <91855027+maxa2@users.noreply.github.com>
2023-10-17 12:14:49 -0700

    Worked on angular cognito integration

---
+++ app/angular-cognito/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    cognito: {
+    userPoolId: 'ca-central-1_ZROplm3Ie',
+    userPoolWebClientId: '39fj6c7ose3j5426adh5ar8lu7'
+    
+  }
+
+};
+
+
