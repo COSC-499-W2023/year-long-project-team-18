132f68e3b464812992cd5dc7285901911ad735b1
Sambhav Garg <78362105+sam131102@users.noreply.github.com>
2024-01-19 12:19:47 -0800

    Changed the date format and VideoName function

--- app/angular-cognito/src/app/video-listing.service.ts
+++ app/angular-cognito/src/app/video-listing.service.ts
@@ -30,15 +30,27 @@ export class VideoListingService {
   }
 
   private extractVideoName(videoKey: string): string {
-    const parts = videoKey.split('-');
-    return parts.slice(0, -1).join('-');
+    const lastSlashIndex = videoKey.lastIndexOf('/');
+    return lastSlashIndex !== -1 ? videoKey.slice(lastSlashIndex + 1) : videoKey;
   }
-
-  private extractVideoDate(videoKey: string): string {
-    const parts = videoKey.split('-');
-    return parts[parts.length - 1].split('.')[0];
+  
+  private extractVideoDate(videoKey: string, timestampFromMetadata: number): string {
+    let timestamp = timestampFromMetadata || 0; 
+  
+    if (timestamp) {
+      const dateObj = new Date(timestamp);
+      const formattedDate = `${dateObj.getUTCFullYear()}-${this.padZero(dateObj.getUTCMonth() + 1)}-${this.padZero(dateObj.getUTCDate())} ${this.padZero(dateObj.getUTCHours())}:${this.padZero(dateObj.getUTCMinutes())}:${this.padZero(dateObj.getUTCSeconds())}`;
+      return formattedDate;
+    } else {
+      console.error('Invalid timestamp:', timestamp);
+      return '';
+    }
   }
-
+  
+  private padZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }  
+  
   getVideos(): Observable<VideoMetadata[]> {
     return from(this.cognitoService.getUsername() as Promise<string>).pipe(
       switchMap(username => {
@@ -64,11 +76,12 @@ export class VideoListingService {
             }
   
             return data.Contents
-              .filter((item: any) => !item.Key.endsWith('/')) // Exclude folders
+              .filter((item: any) => !item.Key.endsWith('/'))
               .map((item: any) => {
                 const videoKey = item.Key;
                 const videoName = this.extractVideoName(videoKey);
-                const videoDate = this.extractVideoDate(videoKey);
+                const timestampFromMetadata = item.LastModified && item.LastModified.getTime();
+                const videoDate = this.extractVideoDate(videoKey, timestampFromMetadata);
                 return {
                   key: videoKey,
                   name: videoName,
