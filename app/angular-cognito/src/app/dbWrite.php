f4e08b42a66b13b19931145f90bdc082aa84d521
maxa2 <91855027+maxa2@users.noreply.github.com>
2024-02-06 12:33:55 -0800

    added php scripts

--- app/angular-cognito/src/app/dbWrite.php
+++ app/angular-cognito/src/app/dbWrite.php
@@ -1,6 +1,29 @@
 <?php
 
+$servername = "prvcy-main-db.cjrkirabesjd.ca-central-1.rds.amazonaws.com";
+$user = "admin";
+$password = "admin499";
+$port = "13306";
+
+$conn = new mysqli ($servername, $user, $password, $port);
+
+$error = mysqli_connect_error();
+
+if($error = null){
+    $output = "<p>There was an issue connecting to the database, please try again later.</p>";
+    exit($output)
+}
+else{
+    $sql = "INSERT INTO Users (Email)
+            values ('test@test.com');";
+
+    $result = mysqli_query($conn, $sql);
 
 
 
->
+
+    mysqli_free_results($result);
+    mysqli_close($connection);
+}
+
+?>
