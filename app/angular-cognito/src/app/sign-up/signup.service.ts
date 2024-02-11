db60358675aadeac9053f22680f1100e761b7bdb
maxa2 <maxa@live.ca>
2024-02-10 23:21:42 -0800

    adding front end compontents of api

---
+++ app/angular-cognito/src/app/sign-up/signup.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs';
+import { signup } from './signup'; 
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+  baseUrl = 'http://localhost/api';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(){
+    return this.http.get('http://localhost/api/list.php',{responseType:'text'}).pipe(
+      map((res: any)=>{
+        return res['data'];
+      })
+    )
+  }
+
+}
