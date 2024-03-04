70d9f96db07c896a0da8eb13ca056f53a0be6a79
maxa2 <91855027+maxa2@users.noreply.github.com>
2024-03-03 16:48:11 -0800

    Update sign-up.component.ts

--- app/angular-cognito/src/app/sign-up/sign-up.component.ts
+++ app/angular-cognito/src/app/sign-up/sign-up.component.ts
@@ -1,32 +1,45 @@
 // sign-up.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, NgForm, Validators } from '@angular/forms';
 import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { IUser, CognitoService } from '../cognito.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+
+import { signup } from './signup';
+import { SignupService } from './signup.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
 
+  signup: signup[] = [];
+  sign: signup = {email: '', username:'', password:'', firstname:'', lastname:'',birthdate:'', organizationcode:'',accounttype:''};
   loading: boolean;
   user: IUser;
   hide = true;
   durationInSeconds = 5;
 
-  constructor(private router: Router, private cognitoService: CognitoService, private snackBar: MatSnackBar) {
+  constructor(private router: Router, private cognitoService: CognitoService, private snackBar: MatSnackBar, private signUpService: SignupService) {
     this.loading = false;
     this.user = {} as IUser;
-    
   }
-  openSnackBar(){
-    this.snackBar.open("Successfully registered","Dismiss", {
-      duration: this.durationInSeconds*1000
-    })
+
+  ngOnInit(): void {
+      this.getSignup();
+  }
+
+  getSignup(): void{
+    this.signUpService.getAll().subscribe(
+      (data: signup[])=>{
+        this.signup = data;
+      }
+    )
+
   }
 
   email = new FormControl('', [Validators.required, Validators.email]);
@@ -34,26 +47,26 @@ export class SignUpComponent {
   /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{8,}$/;
 
   password = new FormControl('', {
-    validators: [Validators.required, Validators.pattern(this.StrongPasswordRegx)],
+    validators: [Validators.required, Validators.pattern(this.StrongPasswordRegx), Validators.minLength(8)],
   })
 
   getPasswordErrorMessage(){
     if(this.password.hasError('required')){
       return 'You must enter a value';
+    }if(this.password.hasError('minlength')){
+      return 'Must be 8 characters';
     }
-    return this.password.hasError('pattern') ? 'Password must contain an uppercase, lowercase and special character':'';
+    return this.password.hasError('pattern') ? 'Password must contain an uppercase, number and special character':'';
   }
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
-
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
-
-   public signUp(username: string, organization: string): void {
+   public signUp(username: string, organization: string, f: NgForm): void {
     this.loading = true;
 
     var bdate = this.user.birthdate.toString();
@@ -107,13 +120,24 @@ export class SignUpComponent {
       this.user['custom:organization'] = 'default';
     }
     this.user.username = username;
-    
+    this.sign = {email: this.user.email, username:this.user.username, password:this.user.password, 
+      firstname:this.user.given_name, lastname:this.user.family_name,birthdate:this.user.birthdate,
+       organizationcode:this.user['custom:organization'],accounttype:this.user['custom:account_type']};
+    this.signUpService.store(this.sign).subscribe(
+      (res: signup)=>{
+        this.signup.push(res)
+      }
+    )
+
     this.cognitoService.signUp(this.user)
       .then(() => {
         this.createS3UserFolder(username);
-        this.router.navigate(['/signIn']);
+        this.subscribeUserToSnsTopic(this.user.email);
+        this.router.navigate(['/signIn']).then(()=>{
+          this.snackBar.open("Successfully registered", "Dismiss",{duration: 5000})
+        })
       })
-      .catch((error) => {
+    .catch((error) => {
         console.error('Sign Up Error:', error);
         this.loading = false;
       });
@@ -135,15 +159,23 @@ export class SignUpComponent {
 
   public confirmSignUp(): void {
     this.loading = true;
-
     this.cognitoService.confirmSignUp(this.user)
+      //I'm not entirely sure this part of the code is accessed at all
       .then((confirmationResult) => {
         console.log('Confirmation result:', confirmationResult);
-        this.router.navigate(['/signIn']);
+        
+        this.router.navigate(['/signIn'])
       })
       .catch((error) => {
         console.error('Confirm Sign Up Error:', error);
         this.loading = false;
       });
   }
+  public subscribeUserToSnsTopic(userEmail: string): void {
+    const topicArn = 'arn:aws:sns:ca-central-1:952490130013:prvcy';
+    this.cognitoService.subscribeUserToSnsTopic(userEmail, topicArn)
+      .then(() => console.log('User subscribed to SNS topic'))
+      .catch((error) => console.error('Error subscribing user to SNS topic:', error));
+  }
+  
 }
